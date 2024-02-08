0f42e7548faff9d94631c3b0399fadfab036ff44
Diego Vidal Lopez <diego27796gagan@gmail.com>
2024-02-07 21:45:00 -0600

    Add status variables

--- src/main.js
+++ src/main.js
@@ -1,1 +1,10 @@
 console.log('Runing Main Js')
+
+let xp = 0;
+let health = 100;
+let gold = 50;
+let currentWeapon = 0;
+let fighting;
+let fighting;
+let monsterHealth;
+let inventory = ["stick"];
