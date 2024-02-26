01fefae682161590ee5470204a0929d14ce4bb50
Diego Vidal Lopez <diego27796gagan@gmail.com>
2024-02-26 16:07:17 -0600

    feat: Initialize buttons 1, 2 and 3.

--- src/main.js
+++ src/main.js
@@ -21,6 +21,11 @@ const monsterName = document.querySelector("#monsterName");
 
 const monsterHealthText = document.querySelector("#monsterHealth");
 
+// initialize buttons
+button1.onclick = goStore;
+button2.onclick = goCave;
+button3.onclick = fightDragon;
+
 function goStore() {
   console.log("Going to store.");
 };
