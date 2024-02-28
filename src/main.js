6b86ffa4492e9a30f2519c5852d345340c5f4110
Diego Vidal Lopez <diego27796gagan@gmail.com>
2024-02-27 21:17:17 -0600

    feat: update text in button2 and button 3

--- src/main.js
+++ src/main.js
@@ -28,6 +28,8 @@ button3.onclick = fightDragon;
 
 function goStore() {
   button1.innerText = "Buy 10 health (10 gold)";
+  button2.innerText = "Buy weapon (30 gold)";
+  button3.innerText = "Go to town square";
 };
 
 function goCave(){
