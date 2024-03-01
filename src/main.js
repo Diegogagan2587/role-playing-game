150b5846d3b5b46deeffc89e582349df29ddb9ff
Diego Vidal Lopez <diego27796gagan@gmail.com>
2024-03-01 08:32:19 -0600

    Add locations const to store town square properties

--- src/main.js
+++ src/main.js
@@ -21,6 +21,13 @@ const monsterName = document.querySelector("#monsterName");
 
 const monsterHealthText = document.querySelector("#monsterHealth");
 
+const locations = [{
+  name: "town square",
+  "button text": ["Go to store","Go to cave", "Fight dragon"],
+  "button functions":[goStore, goCave, fightDragon],
+  text: "You are in the town square. You see a sign that says \"Store\".",
+}];
+
 // initialize buttons
 button1.onclick = goStore;
 button2.onclick = goCave;
