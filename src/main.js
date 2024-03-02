c61cd1d3b5b15aa073077d0c9a2c6d2a3d0054cc
Diego Vidal Lopez <diego27796gagan@gmail.com>
2024-03-02 17:20:16 -0600

    Add new location in locations constant.

--- src/main.js
+++ src/main.js
@@ -26,6 +26,12 @@ const locations = [{
   "button text": ["Go to store","Go to cave", "Fight dragon"],
   "button functions":[goStore, goCave, fightDragon],
   text: "You are in the town square. You see a sign that says \"Store\".",
+},
+{
+  name: "store",
+  "button text": ["Buy 10 health (10 gold)", "Buy weapon (30 gold)", "Go to town square"],
+  "button functions":[buyHealth, buyWeapon, goTown],
+  text: "You enter the store.",
 }];
 
 // initialize buttons
