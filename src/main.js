3980dcdf86077391edae9568a0dffd45a1f63637
Diego Vidal Lopez <diego27796gagan@gmail.com>
2024-03-05 21:14:41 -0600

    Add a new cave location in locations.

--- src/main.js
+++ src/main.js
@@ -32,7 +32,14 @@ const locations = [{
   "button text": ["Buy 10 health (10 gold)", "Buy weapon (30 gold)", "Go to town square"],
   "button functions":[buyHealth, buyWeapon, goTown],
   text: "You enter the store.",
-}];
+},
+{
+  name: "cave",
+  "button text": ["Fight slime", "Fight fanged beast", "Go to town square"],
+  "button functions": [fightSlime, fightBeast, goTown],
+  text: "You enter the cave. You see some monsters."
+}
+];
 
 // initialize buttons
 button1.onclick = goStore;
