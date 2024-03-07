e0ed5173c0d48de1783f9903e51da0ee4a369bbc
Diego Vidal Lopez <diego27796gagan@gmail.com>
2024-03-07 17:37:23 -0600

    feat/weapons: add weapons arrays

--- src/main.js
+++ src/main.js
@@ -20,6 +20,22 @@ const monsterStats = document.querySelector("#monsterStats");
 const monsterName = document.querySelector("#monsterName");
 
 const monsterHealthText = document.querySelector("#monsterHealth");
+const weapons = [{
+  name:"stick",
+  power: 5,
+},
+{
+  name:"dagger",
+  power: 30,
+},
+{
+  name:"claw hammer",
+  power: 50,
+},
+{
+  name:"sword",
+  power: 100,
+},];
 
 const locations = [{
   name: "town square",
