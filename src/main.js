e3d6b50d66e30658fbfdd85ac23a300f62d5b576
Diego Vidal Lopez <diego27796gagan@gmail.com>
2024-03-07 17:32:35 -0600

    feature/buyHealth(): change text inner text if not enough money.

--- src/main.js
+++ src/main.js
@@ -68,7 +68,9 @@ function buyHealth(){
     health += 10;
     goldText.innerText = gold;
     healthText.innerText = health;
-  };
+  } else {
+    text.innerText = "You do not have enough gold to buy health."
+  }
 };
 
 function buyWeapon(){
