ab4b90a675839a4ac39116123b0528200741078e
Diego Vidal Lopez <diego27796gagan@gmail.com>
2024-03-07 17:29:52 -0600

    refactor/buyHealth: if gold is >= 10 then buy.

--- src/main.js
+++ src/main.js
@@ -63,10 +63,12 @@ function fightDragon(){
 }
 
 function buyHealth(){
-  gold -= 10;
-  health += 10;
-  goldText.innerText = gold;
-  healthText.innerText = health;
+  if(gold >= 10) {
+    gold -= 10;
+    health += 10;
+    goldText.innerText = gold;
+    healthText.innerText = health;
+  };
 };
 
 function buyWeapon(){
