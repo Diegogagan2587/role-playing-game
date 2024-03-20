b0f8a79fdf32bef71b8a1ac4f18c06f1da4a3884
Diego Vidal Lopez <diego27796gagan@gmail.com>
2024-03-19 20:44:26 -0600

    Add sell Weapon when user already have most powerfull weapon.

--- src/main.js
+++ src/main.js
@@ -104,6 +104,8 @@ function buyWeapon(){
     };
   } else {
     text.innerText = "You already have the most powerful weapon!"
+    button2.innerText = "Sell weapon for 15 gold"
+    button2.onclick = sellWeapon();
   }
   
 };
