f9b3c7bab836cbcd7b8786ad2e06b8da0c2ee949
Diego Vidal Lopez <diego27796gagan@gmail.com>
2024-03-20 21:37:28 -0600

    feat/sellWeapon(): update text.innerText

--- src/main.js
+++ src/main.js
@@ -123,6 +123,7 @@ function sellWeapon(){
     gold += 15;
     goldText.innerText = gold;
     let currentWeapon = inventory.shift();
+    text.innerText = "You sold a "+currentWeapon+".";
   }
 };
 
