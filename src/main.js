9ad7f06105b2753fbd6a3bbd48a100628d03c2e0
Diego Vidal Lopez <diego27796gagan@gmail.com>
2024-03-21 17:26:02 -0600

    feat/main.js: create an array of monsters objects with 3 items.

--- src/main.js
+++ src/main.js
@@ -37,6 +37,22 @@ const weapons = [{
   power: 100,
 },];
 
+const monsters = [{
+  name:"slime",
+  level:2,
+  health:15,
+},
+{
+  name:"fanged beast",
+  level:8,
+  health:60,
+},
+{
+  name:"dragon",
+  level:20,
+  health:300,
+}];
+
 const locations = [{
   name: "town square",
   "button text": ["Go to store","Go to cave", "Fight dragon"],
